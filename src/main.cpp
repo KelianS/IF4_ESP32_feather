0ef2068be006868b69770100b8480c2d0bc8ceee
KelianS <kelian.2002@live.fr>
2022-05-18 09:58:25 +0200

    interrupt and tick count

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
+#define BP_S1 digitalPinToInterrupt(GPIO_NUM_14)
+
+//Global variables
+int LedState = LOW;
+TickType_t LastInterruptTick = 0;
+
+void IRAM_ATTR my_isr();
 
 void setup() {
   // Configure IOs
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Configure Interrupt
+  attachInterrupt(BP_S1, my_isr, FALLING);
 
   //Configure Serial
   Serial.begin(115200);
@@ -12,12 +22,19 @@ void setup() {
 void loop() {
 
   //Serial Hello World
-  Serial.println("Hello");
+  //Serial.println("Hello");
   delay(3000);
 
-  // Blink Led
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+}
+
+
+void IRAM_ATTR my_isr() {
+  TickType_t tickInISR = xTaskGetTickCountFromISR();
+  
+  if (pdTICKS_TO_MS(tickInISR) > pdTICKS_TO_MS(LastInterruptTick)+200) {
+    LastInterruptTick = tickInISR;
+    LedState = !LedState;
+    digitalWrite(LED_BUILTIN, LedState);
+    //Serial.println(LedState); //Degub
+  }
 }
