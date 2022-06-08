1cca1ed61fe8f6151435272ae436a576aaae5ee0
KelianS <kelian.2002@live.fr>
2022-06-08 09:12:10 +0200

    creation 2 taches

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,87 @@
+//Kélian SERMET
+//08/06/2022
+//http://insa.pages.univ-lyon1.fr/freertos/activites/tp1/
+
+
 #include <Arduino.h>
 #define BP_S1 digitalPinToInterrupt(GPIO_NUM_14)
+#define LED_GREEN 19
+#define LED_BLUE 4
 
-//Global variables
-int LedState = LOW;
-TickType_t LastInterruptTick = 0;
 
-void IRAM_ATTR my_isr();
+
+#if CONFIG_FREERTOS_UNICORE
+#define APP_CORE 0
+#else
+#define APP_CORE (portNUM_PROCESSORS - 1)
+#endif
+
+
+
+void LED_builtin_blink(void *pvParameters);
+void LED_blue_blink(void *pvParameters);
+
 
 void setup() {
-  // Configure IOs
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  // Configure Interrupt
-  attachInterrupt(BP_S1, my_isr, FALLING);
-
   //Configure Serial
   Serial.begin(115200);
   while(!Serial);
+
+  //RTOS Priority
+  Serial.print("Task : "); Serial.print(pcTaskGetName(NULL)); Serial.print(" - Coeur : ");  Serial.print(xPortGetCoreID()); Serial.print(" - Priority : "); Serial.println(uxTaskPriorityGet(NULL));
+  
+
+  xTaskCreatePinnedToCore(LED_builtin_blink,
+                          "LED_BUILTIN_TASK",
+                          1024,
+                          nullptr,
+                          1,
+                          NULL,
+                          1);
+
+  xTaskCreatePinnedToCore(LED_blue_blink,
+                          "LED_BLUE_TASK",
+                          1024,
+                          nullptr,
+                          1,
+                          NULL,
+                          1);
+
 }
 
 void loop() {
-
-  //Serial Hello World
-  //Serial.println("Hello");
-  delay(3000);
-
+  delay(2000);
+  Serial.print("Task : "); Serial.print(pcTaskGetName(NULL)); Serial.print(" - Coeur : ");  Serial.print(xPortGetCoreID()); Serial.print(" - Priority : "); Serial.println(uxTaskPriorityGet(NULL));
+  
 }
 
 
-void IRAM_ATTR my_isr() {
-  TickType_t tickInISR = xTaskGetTickCountFromISR();
-  
-  if (pdTICKS_TO_MS(tickInISR) > pdTICKS_TO_MS(LastInterruptTick)+200) {
-    LastInterruptTick = tickInISR;
+
+
+void LED_builtin_blink(void *pvParameters){
+  Serial.print("Task : "); Serial.print(pcTaskGetName(NULL)); Serial.print(" - Coeur : ");  Serial.print(xPortGetCoreID()); Serial.print(" - Priority : "); Serial.println(uxTaskPriorityGet(NULL));
+
+  // Configure IOs
+  pinMode(LED_BUILTIN, OUTPUT);
+  int LedState = LOW;
+
+  for(;;){
     LedState = !LedState;
     digitalWrite(LED_BUILTIN, LedState);
-    //Serial.println(LedState); //Degub
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
+void LED_blue_blink(void *pvParameters){
+  Serial.print("Task : "); Serial.print(pcTaskGetName(NULL)); Serial.print(" - Coeur : ");  Serial.print(xPortGetCoreID()); Serial.print(" - Priority : "); Serial.println(uxTaskPriorityGet(NULL));
+  
+  // Configure IOs
+  pinMode(LED_BLUE, OUTPUT);
+  int LedStateBlue = LOW;
+
+  for(;;){
+    LedStateBlue = !LedStateBlue;
+    digitalWrite(LED_BLUE, LedStateBlue);
+    vTaskDelay(pdMS_TO_TICKS(1500));
   }
 }
