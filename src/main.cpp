47ee3836e7a65c254605508987affe057cb08064
KelianS <kelian.2002@live.fr>
2022-06-10 16:42:28 +0200

    Bouton poussoir anti rebond avec gestion file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 //Kélian SERMET
 //10/06/2022
-//http://insa.pages.univ-lyon1.fr/freertos/activites/tp2/
+//http://insa.pages.univ-lyon1.fr/freertos/activites/tp3/
+
 
 #include <Arduino.h>
 
@@ -19,105 +20,90 @@ const int LED_RED   = 16;  // RED LED corresponds to GPIO16
 const int LED_GREEN = 19;  // GREEN LED corresponds to GPIO19
 const int LED_BLUE  = 4;   // BLUE LED corresponds to GPIO4
 
-// Prototypes
-void TaskBlink( void *pvParameters );
-void TaskBlinkPeriodic( void *pvParameters );
 
+// Buttons
+#define  BP_1_PIN    14
+#define  BP_2_PIN    15
+
+//Queue
+static QueueHandle_t queue;
+
+
+
+static void DebounceTask(void *pvParameters) {
+  uint32_t level, state = 0, last = 1;
+  uint32_t mask = 0x7FFFFFFF;
+  bool event;
+  Serial.println("Debounce TASK start");
+
+  for (;;) {
+    level = digitalRead(BP_1_PIN);
+    state = (state << 1) | level;
+    if (((state & mask) == mask) || ((state & mask) == 0)) {
+      if (level != last) {
+        event = !level;  // true when pressed
+        if (uxQueueSpacesAvailable(queue)!=0) {       //send event to Queue and only modify last level if the event could be sent
+          xQueueSendToBack(queue, (void *)&event, portMAX_DELAY);
+          last = level;
+        }
+      }
+    }
+    vTaskDelay(pdMS_TO_TICKS(2));
+  }
+}
+
+static void LedTask(void *pvParameters) {
+  bool event, led = false;
+  Serial.println("Led TASK start");
+
+  digitalWrite(LED_BUILTIN, led);
+
+  for (;;) {
+    xQueueReceive(queue, (void *)&event, portMAX_DELAY);     //Read from queue
+    if (event) {
+      led ^= true;
+      digitalWrite(LED_BUILTIN, led);
+    } else {
+      for(int i=1;i<=10;i++){ //Flash 10 pulses on Blue LED
+        digitalWrite(LED_BLUE,HIGH);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        digitalWrite(LED_BLUE,LOW);
+        vTaskDelay(pdMS_TO_TICKS(200));
+      }    
+    }
+  }
+}
 
 void setup() {
 
   Serial.begin(115200);
   while(!Serial);
-  
-  xTaskCreatePinnedToCore(
-    TaskBlink,  //Function
-    "Blink red",       //Name
-    2048,               //Stack size
-    (void*)&LED_RED,    //Parameters
-    5,                  //Priority
-    nullptr,            //Task handle
-    APP_CORE);          //Core
+  Serial.println("BEGIN");
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(BP_1_PIN, INPUT);
 
+  //...         //Creation de la file
+  queue = xQueueCreate(10, sizeof(bool));
 
+  xTaskCreatePinnedToCore(DebounceTask,  // Function
+                          "Debounce",    // Name
+                          8192,          // Stack size
+                          nullptr,       // Parameters
+                          2,             // Priority
+                          nullptr,       // Task handle
+                          APP_CORE);     // Core
 
-
-  xTaskCreatePinnedToCore(
-    TaskBlink,  //Function
-    "Blink green",       //Name
-    2048,               //Stack size
-    (void*)&LED_GREEN,    //Parameters
-    4,                  //Priority
-    nullptr,            //Task handle
-    APP_CORE);          //Core
-
-    vTaskDelay(pdMS_TO_TICKS(1));
-
-  xTaskCreatePinnedToCore(
-    TaskBlink,  //Function
-    "Blink blue",       //Name
-    2048,               //Stack size
-    (void*)&LED_BLUE,    //Parameters
-    5,                  //Priority
-    nullptr,            //Task handle
-    APP_CORE);          //Core
-
+  xTaskCreatePinnedToCore(LedTask,       // Function
+                          "Toggle Led",  // Name
+                          8192,          // Stack size
+                          nullptr,       // Parameters
+                          1,             // Priority
+                          nullptr,       // Task handle
+                          APP_CORE);     // Core
 }
 
-void loop()
-{
+void loop() {
   // Tout est fait dans les tâches
   vTaskDelete(nullptr);
-}
-
-
-/*********** Analyse de l'ordonnancement Round Robin avec 3 taches ******************/
-void TaskBlink(void *pvParameters) 
-{
-  bool shouldWait = true;
-  //Retrieve Pin number
-  uint8_t pin = *((uint8_t*)pvParameters);
-
-  Serial.printf("Starting on : %hhd \n", pin);
-  vTaskDelay(pdMS_TO_TICKS(2));
-
-  pinMode(pin, OUTPUT);
-
-  for (;;) {
-    for (uint32_t i = 0; i < 40000; i++) {
-      digitalWrite(pin, HIGH);
-      digitalWrite(pin, LOW);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-
-
-
-
-
-/*********** vTaskDelay Vs vTaskDelayUntil ******************/
-void TaskBlinkPeriodic(void *pvParameters) 
-{
-  bool shouldWait = true;
-  //Retrieve Pin number
-  uint8_t pin = *((uint8_t*)pvParameters);
-  pinMode(pin, OUTPUT);
-  //vTaskDelayUntil()
-  
-  for (;;)
-  {
-    digitalWrite(pin, HIGH);
-    vTaskDelay(pdMS_TO_TICKS(200));
-    
-    //Add jitter
-    if(shouldWait) {
-      vTaskDelay(pdMS_TO_TICKS(50));
-      shouldWait = false;  
-    } else {
-      shouldWait = true;
-    }
-    digitalWrite(pin, LOW);
-    vTaskDelay(pdMS_TO_TICKS(400));
-  }
 }
