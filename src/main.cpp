fd5ef0d42ebc71cef0e04c2e1185abb9fed1ecad
KelianS <kelian.2002@live.fr>
2022-06-14 21:43:58 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,95 @@
 //Kélian SERMET
-//10/06/2022
-//http://insa.pages.univ-lyon1.fr/freertos/activites/tp3/
-
+//14/06/2022
+//http://insa.pages.univ-lyon1.fr/freertos/activites/tp4/
 
 #include <Arduino.h>
+
+#if CONFIG_FREERTOS_UNICORE
+#define APP_CORE 0
+#else
+#define APP_CORE (portNUM_PROCESSORS - 1)
+#endif
+
+// Task function prototype
+void waitingTask(void *pvParameters);
+
+#define PUSH_BUTTON_S1 14
+
+//A compléter
+
+void IRAM_ATTR toggleLED() {
+  static BaseType_t xLastTick = 0;
+  BaseType_t xCurrentTick;
+  //Déclaration de variable à compléter
+
+  xCurrentTick = xTaskGetTickCountFromISR();
+  if (pdTICKS_TO_MS(xCurrentTick - xLastTick) > 200) {
+    //A compléter
+  }
+  xLastTick = xCurrentTick;
+  
+  //A compléter
+}
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial) {}
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PUSH_BUTTON_S1, INPUT);
+  
+  syncSema = xSemaphoreCreateBinary();
+
+  xTaskCreatePinnedToCore(
+    waitingTask,
+    "Waiting task",
+    8192,
+    nullptr,
+    5,
+    nullptr,
+    APP_CORE
+  );
+
+  attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON_S1), toggleLED, ...);       //A compléter
+}
+
+void loop() { 
+    vTaskDelete(nullptr); 
+}
+
+void waitingTask(void *pvParameters) {
+
+  for (;;) {
+    //A Compléter
+    Serial.println("Task did wake up");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/****************** OLD TO DELETE ***********************/
 
 #if CONFIG_FREERTOS_UNICORE
 #define APP_CORE 0
